ed588036cd85d66bc7a37623a98c09d69a0bd925
Matthew Dang <dang.its.matthew@gmail.com>
2014-03-20 23:39:45 +1100

    random colours!

--- the-wall.js
+++ the-wall.js
@@ -1,6 +1,8 @@
+Squares = new Meteor.Collection('squares');
+
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
-    return "Welcome to the-wall.";
+    return "Welcome to the wall.";
   };
 
   Template.hello.events({
@@ -10,10 +12,34 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
+  Template.wall.squares = function() {
+    return Squares.find();
+  };
+
 }
 
+var WIDTH = 40;
+var HEIGHT = 20;
+
 if (Meteor.isServer) {
+  var insertAllSquares = function() {
+    if (Squares.find().count() === 0) {
+      for (var i = 0; i < WIDTH; i++) {
+        for (var j = 0; j < HEIGHT; j++) {
+          Squares.insert({
+            x: i,
+            y: j,
+            color: Random.hexString(6)
+          });
+        };
+      };
+    }
+  };
+
   Meteor.startup(function () {
     // code to run on server at startup
+
+    insertAllSquares();
   });
 }
